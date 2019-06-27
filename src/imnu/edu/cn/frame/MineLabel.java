666d355eb7512a2aeaf4a91ec44642fd218e0596
Ciec20171106523 <43673487+Ciec20171106523@users.noreply.github.com>
2019-06-27 16:39:11 +0800

    JAVA_Minegame

--- src/imnu/edu/cn/frame/MineLabel.java
+++ src/imnu/edu/cn/frame/MineLabel.java
@@ -5,6 +5,49 @@ import javax.swing.JLabel;
 public class MineLabel extends JLabel{
      private int rowIndex;
      private int colIndex;
+     private boolean isMine;
+     private boolean isExpanded;
+     private boolean isFlag;
+     private boolean isMineAndNotflag;
+     private int mineCount;
+     private int rightClickCount;
+     
+	public boolean isMine() {
+		return isMine;
+	}
+	public void setMine(boolean isMine) {
+		this.isMine = isMine;
+	}
+	public boolean isExpanded() {
+		return isExpanded;
+	}
+	public void setExpanded(boolean isExpanded) {
+		this.isExpanded = isExpanded;
+	}
+	public boolean isFlag() {
+		return isFlag;
+	}
+	public void setFlag(boolean isFlag) {
+		this.isFlag = isFlag;
+	}
+	public boolean isMineAndNotflag() {
+		return isMineAndNotflag;
+	}
+	public void setMineAndNotflag(boolean isMineAndNotflag) {
+		this.isMineAndNotflag = isMineAndNotflag;
+	}
+	public int getMineCount() {
+		return mineCount;
+	}
+	public void setMineCount(int mineCount) {
+		this.mineCount = mineCount;
+	}
+	public int getRightClickCount() {
+		return rightClickCount;
+	}
+	public void setRightClickCount(int rightClickCount) {
+		this.rightClickCount = rightClickCount;
+	}
 	public int getRowIndex() {
 		return rowIndex;
 	}
@@ -22,4 +65,5 @@ public class MineLabel extends JLabel{
 		this.colIndex=colIndex;
 		// TODO Auto-generated constructor stub
 	}
+	
 }
--- src/imnu/edu/cn/frame/MineSweeper.java
+++ src/imnu/edu/cn/frame/MineSweeper.java
@@ -5,6 +5,8 @@ import java.awt.GridLayout;
 
 import javax.swing.JPanel;
 
+
+
 import imnu.edu.cn.tools.tools;
 
 public class MineSweeper extends JPanel{
@@ -39,7 +41,32 @@ public class MineSweeper extends JPanel{
     	}
     	this.add(jPanel2);
 	}
+	public void buildMine(int rowx,int coly) {
+		for(int i=0;i<tools.totalMine;i++) {
+			int x=(int)(Math.random()*tools.totalx);
+			int y=(int)(Math.random()*tools.totaly);			
+			mineLabel[x][y].setMine(true);
+		}
+		
+			for (int i = 0; i < tools.totalx; i++) {
+				for (int j = 0; j < tools.totaly; j++) {
+					 int count = 0;
+					if (!mineLabel[i][j].isMine()) {
+						for (int x = Math.max(i - 1, 0); x <= Math.min(i + 1,
+								tools.totalx - 1); x++) {
+							for (int y = Math.max(j - 1, 0); y <= Math.min(j + 1,
+									tools.totaly - 1); y++) {
+								if (mineLabel[x][y].isMine())
+									count++;
+							}
+						}
+						mineLabel[i][j].setMineCount(count);						
+					}
+				}
+			}
+       }
+   }
+
 	
 	
-	
-}
+
